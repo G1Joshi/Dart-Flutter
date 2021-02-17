16fb95752d9380533cfc04aaaa01eff418454d29
G1Joshi <joshijeevan98@gmail.com>
2021-02-17 08:06:30 +0530

    hello_world

--- Flutter/hello_world.dart
+++ Flutter/hello_world.dart
@@ -6,29 +6,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Hello World'),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Hello World',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({@required this.title});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text(title),
+        backgroundColor: Colors.grey[800],
+        title: Text('Flutter Hello World'),
+        centerTitle: true,
+        elevation: 0.0,
       ),
       body: Center(
         child: Text(
           'Hello, World!',
+          style: TextStyle(
+            color: Colors.grey[100],
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+            letterSpacing: 2.0,
+          ),
         ),
       ),
     );
