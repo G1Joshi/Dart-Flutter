53847c2c384df339418899aaffc35e693c8dc3ea
G1Joshi <joshijeevan98@gmail.com>
2021-03-05 22:17:17 +0530

    hello world

---
+++ Flutter/hello_world/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Hello World',
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  bool eng = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[800],
+        title: Text('Flutter Hello World'),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Text(
+          eng ? 'Hello, World!' : 'हैलो, वर्ल्ड!',
+          style: TextStyle(
+            color: Colors.grey[100],
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+            letterSpacing: 2.0,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey[800],
+        onPressed: () {
+          setState(() {
+            eng = !eng;
+          });
+        },
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
